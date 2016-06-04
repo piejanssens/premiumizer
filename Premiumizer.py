95ea8bd4f7be6314bb3fa45ba8b441d340e72b74
neox387 <neox387@gmail.com>
2016-06-04 14:56:36 +0200

    change deprecated import for flask_login/socketio

--- Premiumizer.py
+++ Premiumizer.py
@@ -25,8 +25,8 @@ import six
 from apscheduler.schedulers.gevent import GeventScheduler
 from chardet import detect
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
-from flask.ext.login import LoginManager, login_required, login_user, logout_user, UserMixin
-from flask.ext.socketio import SocketIO, emit
+from flask_login import LoginManager, login_required, login_user, logout_user, UserMixin
+from flask_socketio import SocketIO, emit
 from flask_apscheduler import APScheduler
 from gevent import local
 from pySmartDL import SmartDL, utils
