53ccdab64a0b16938d72148530a582333fef64d8
neox387 <neox387@gmail.com>
2017-08-21 22:42:37 +0200

    fix download bug

--- premiumizer.py
+++ premiumizer.py
@@ -796,7 +796,7 @@ def download_process():
     payload = {'customer_id': cfg.prem_customer_id, 'pin': cfg.prem_pin,
                'hash': greenlet.task.hash}
     r = prem_connection("post", "https://www.premiumize.me/api/torrent/browse", payload)
-    if 'failed' not in r:
+    if 'failed' in r:
         return 1
     greenlet.task.update(local_status='downloading', progress=0, speed=' ', eta=' ')
     greenlet.task.dldir = os.path.join(greenlet.task.dldir, clean_name(greenlet.task.name))
