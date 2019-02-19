6a1b71c3b834dcb347efb8744efd2fc8a1c6153d
Adrian Neubauer <AdrianNeubauer@gmx.de>
2019-02-19 17:17:26 +0100

    this fixes a start-up issue with the latest restructuring

--- premiumizer/docker-entrypoint.sh
+++ premiumizer/docker-entrypoint.sh
@@ -6,4 +6,4 @@ PGID=${PGID:-6006}
 groupmod -o -g "$PGID" premiumizer
 usermod -o -u "$PUID" premiumizer
 
-su - premiumizer -p -c 'python premiumizer.py'
+su - premiumizer -p -c 'python ./premiumizer/premiumizer.py'
