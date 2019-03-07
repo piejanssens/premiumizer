1a47aedcc8951ad12f1c49d21a0699d165ddbd35
Pieter Janssens <piejanssens@users.noreply.github.com>
2019-03-07 15:17:48 +0100

    Symlink conf directory to /conf mount

--- premiumizer/docker-entrypoint.sh
+++ premiumizer/docker-entrypoint.sh
@@ -6,4 +6,6 @@ PGID=${PGID:-6006}
 groupmod -o -g "$PGID" premiumizer
 usermod -o -u "$PUID" premiumizer
 
+ln -sf ./premiumizer/conf /conf
+
 su - premiumizer -p -c 'python ./premiumizer/premiumizer.py'
