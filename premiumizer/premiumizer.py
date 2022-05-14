piejanssens/premiumizer

commit 01c5bd79035b2653f41ad8f1bac19c5c1c68c452

Update (#322)
* Add name matching for API id mismatch #320
fix crash enabling blackhole without folder set #321
bump dependencies

* fix crash enabling blackhole without folder set 2 #321

premiumizer/premiumizer.py
@@ -314,7 +314,6 @@ class PremConfig:
         self.web_login_enabled = prem_config.getboolean('security', 'login_enabled')
         self.web_username = prem_config.get('security', 'username')
         self.web_password = prem_config.get('security', 'password')
-
         self.update_available = 0
         self.update_localcommit = ''
         self.update_diffcommit = ''
@@ -412,11 +411,16 @@ class PremConfig:
         self.watchdir_enabled = prem_config.getboolean('upload', 'watchdir_enabled')
         self.watchdir_location = prem_config.get('upload', 'watchdir_location')
         if self.watchdir_enabled:
-            self.watchdir_walk_enabled = prem_config.getboolean('upload', 'watchdir_walk_enabled')
-            self.watchdir_walk_interval = prem_config.getint('upload', 'watchdir_walk_interval')
-            logger.info('Watchdir is enabled at: %s', self.watchdir_location)
-            if not os.path.exists(self.watchdir_location):
-                os.makedirs(self.watchdir_location)
+            if not self.watchdir_location:
+                logger.info('Watchdir location is empty, disabling watchdir')
+                self.watchdir_enabled = 0
+            else:
+                self.watchdir_walk_enabled = prem_config.getboolean('upload', 'watchdir_walk_enabled')
+                self.watchdir_walk_interval = prem_config.getint('upload', 'watchdir_walk_interval')
+                if not os.path.exists(self.watchdir_location):
+                    os.makedirs(self.watchdir_location)
+                logger.info('Watchdir is enabled at: %s', self.watchdir_location)
+
 
         self.categories = []
         self.download_categories = ''
@@ -689,6 +693,7 @@ class MyHandler(events.PatternMatchingEventHandler):
     # noinspection PyMethodMayBeStatic
     def process(self, event):
         failed = 0
+        name2 = ''
         if event.event_type == 'created' and event.is_directory is False:
             gevent.sleep(10)
             watchdir_file = event.src_path
@@ -704,7 +709,7 @@ class MyHandler(events.PatternMatchingEventHandler):
             else:
                 category = ''
             if watchdir_file.endswith('.torrent'):
-                id = upload_torrent(watchdir_file)
+                id, name2 = upload_torrent(watchdir_file)
                 if id == 'duplicate':
                     failed = 1
                     logger.debug(
@@ -719,6 +724,9 @@ class MyHandler(events.PatternMatchingEventHandler):
                     failed = 1
                 else:
                     name = torrent_metainfo(watchdir_file)
+                    #
+                    if name2:
+                        name = name2
                     type = 'Torrent'
                     task = add_task(id, 0, name, category, type=type)
             elif watchdir_file.endswith('.magnet'):
@@ -733,7 +741,10 @@ class MyHandler(events.PatternMatchingEventHandler):
                         except AttributeError:
                             name = os.path.basename(watchdir_file).replace(".magnet", "")
                         name = name.replace('+', '%20')
-                        id = upload_magnet(magnet)
+                        id, name2 = upload_magnet(magnet)
+                        #
+                        if name2:
+                            name = name2
                         if id == 'duplicate':
                             failed = 1
                             logger.debug('Deleting duplicate file from watchdir: %s', watchdir_file)
@@ -767,6 +778,9 @@ class MyHandler(events.PatternMatchingEventHandler):
                 else:
                     name = os.path.basename(watchdir_file)
                     name = os.path.splitext(name)[0]
+                    #
+                    if name2:
+                        name = name2
                     type = 'NZB'
                     task = add_task(id, 0, name, category, type=type)
             else:
@@ -1631,7 +1645,7 @@ def parse_tasks(transfers):
         eta = ' '
         speed = ' '
         size = ' '
-        task = get_task(transfer['id'])
+        task = get_task(transfer['id'], name)
         try:
             if 'ETA is' in transfer['message']:
                 eta = transfer['message'].split("ETA is", 1)[1]
@@ -1672,7 +1686,7 @@ def parse_tasks(transfers):
                 add_task(transfer['id'], size, name, 'default', folder_id=folder_id)
             else:
                 add_task(transfer['id'], size, name, '', folder_id=folder_id)
-            task = get_task(transfer['id'])
+            task = get_task(transfer['id'], name)
             id_local.append(task.id)
             task.update(name=name, progress=progress, cloud_status=transfer['status'], dlsize=size + ' --- ',
                         speed=speed + ' --- ', eta=eta, file_id=file_id)
@@ -1746,7 +1760,7 @@ def parse_tasks(transfers):
         else:
             if task.local_status == 'downloading':
                 if task.name not in str(scheduler.scheduler.get_jobs('check_downloads')):
-                    scheduler.scheduler.add_job(check_downloads, args=(task.dlsize, task.id), id=task.id,
+                    scheduler.scheduler.add_job(check_downloads, args=(task.dlsize, task.id, task.name), id=task.id,
                                                 name=(task.name + ' check_downloads'), misfire_grace_time=7200,
                                                 jobstore='check_downloads', replace_existing=True, max_instances=1,
                                                 coalesce=True, next_run_time=(datetime.now() + timedelta(minutes=10)))
@@ -1783,10 +1797,10 @@ def parse_tasks(transfers):
     return idle
 
 
-def check_downloads(dlsize, id):
+def check_downloads(dlsize, id, name):
     logger.debug('def check_downloads started')
     try:
-        task = get_task(id)
+        task = get_task(id, name)
     except:
         return
     if task.local_status == 'downloading':
@@ -1800,10 +1814,13 @@ def check_downloads(dlsize, id):
             scheduler.scheduler.reschedule_job('update', trigger='interval', seconds=1)
 
 
-def get_task(id):
+def get_task(id, name=None):
     logger.debug('def get_task started')
     for task in tasks:
         if task.id == id:
+            return task
+        elif task.name == name:
+            task.update(id=id)
             return task
     return None
 
@@ -1832,7 +1849,7 @@ def get_cat_var(category):
 
 def add_task(id, size, name, category, type='', folder_id=None):
     logger.debug('def add_task started')
-    exists = get_task(id)
+    exists = get_task(id, name)
     if not exists:
         dldir, dlext, dlext_blacklist, delsample, dlnzbtomedia = get_cat_var(category)
         try:
@@ -1868,7 +1885,7 @@ def upload_torrent(torrent):
         response_content = json.loads(r.content)
         if response_content['status'] == "success":
             logger.debug('Upload successful: %s', torrent)
-            return response_content['id']
+            return response_content['id'], response_content['name']
         elif response_content['message'] == \
                 'An error occured. Please try again and contact customer service if the problem persists.':
             gevent.sleep(10)
@@ -1876,18 +1893,18 @@ def upload_torrent(torrent):
             if 'failed' not in r:
                 response_content = json.loads(r.content)
                 if response_content['status'] == \
-                        "success" or response_content['message'] == 'You already have this job added.':
+                        "success" or response_content['message'] == 'You already added this job.':
                     logger.debug('Upload successful: %s', torrent)
-                    return response_content['id']
+                    return response_content['id'], response_content['name']
         else:
             msg = 'Upload of torrent: %s failed, message: %s' % (torrent, response_content['message'])
             logger.error(msg)
             if response_content['message'] == 'You already have this job added.':
-                return 'duplicate'
+                return 'duplicate', 'duplicate'
             send_notification('Upload of torrent failed', msg)
-            return 'failed'
+            return 'failed', 'duplicate'
     else:
-        return 'failed'
+        return 'failed', 'duplicate'
 
 
 def upload_magnet(magnet):
@@ -1898,7 +1915,7 @@ def upload_magnet(magnet):
         response_content = json.loads(r.content)
         if response_content['status'] == "success":
             logger.debug('Upload magnet successful')
-            return response_content['id']
+            return response_content['id'], response_content['name']
         elif response_content[
             'message'] == 'An error occured. Please try again and contact customer service if the problem persists.':
             gevent.sleep(10)
@@ -1908,7 +1925,7 @@ def upload_magnet(magnet):
                 if response_content['status'] == "success" or response_content[
                     'message'] == 'You already have this job added.':
                     logger.debug('Upload magnet successful')
-                    return response_content['id']
+                    return response_content['id'], response_content['name']
         else:
             msg = 'Upload of magnet: %s failed, message: %s' % (magnet, response_content['message'])
             logger.error(msg)
@@ -2255,7 +2272,7 @@ def settings():
                 prem_config.set('downloads', 'jd_enabled', '0')
             else:
                 prem_config.set('downloads', 'aria2_enabled', '0')
-            if request.form.get('watchdir_enabled'):
+            if request.form.get('watchdir_enabled') and request.form.get('watchdir_location'):
                 prem_config.set('upload', 'watchdir_enabled', '1')
                 if not cfg.watchdir_enabled:
                     enable_watchdir = 1
@@ -2282,7 +2299,6 @@ def settings():
                 prem_config.set('update', 'auto_update', '1')
             else:
                 prem_config.set('update', 'auto_update', '0')
-
             if request.form.get('apprise_enabled'):
                 prem_config.set('notifications', 'apprise_enabled', '1')
             else:
@@ -2344,7 +2360,7 @@ def settings():
             logger.info('Settings saved, reloading configuration')
             cfg.check_config()
             if enable_watchdir:
-                watchdir()
+                gevent.spawn_later(2, watchdir)
             flash('settings saved', 'info')
     # get_prem_folders()
     categories_amount = len(cfg.download_categories) + 1
@@ -2590,7 +2606,7 @@ def handle_json(json):
 @socketio.on('change_category')
 def change_category(message):
     data = message['data']
-    task = get_task(data['id'])
+    task = get_task(data['id'], data['name'])
     dldir, dlext, dlext_blacklist, delsample, dlnzbtomedia = get_cat_var(data['category'])
     if task.type == 'Filehost':
         if task.local_status != 'failed: Filehost':

premiumizer/settings.cfg.tpl
@@ -103,4 +103,4 @@ updated = 1
 auto_update = 0
 update_date = Never
 config_version = 2.9
-req_version = 10.2
+req_version = 10.3

premiumizer/static/js/control.js
@@ -28,10 +28,11 @@ function show_premiumize_connect_error() {
     $('#main_container').attr('style', 'display: none');
 }
 
-function category_selected(id, category) {
+function category_selected(id, name, category) {
     socket.emit('change_category', {
         data: {
             id: id,
+            name: name,
             category: category
         }
     });
@@ -157,7 +158,7 @@ function update_task(task) {
     var dropDown = '<div class="dropdown"><button class="btn btn-xs btn-primary dropdown-toggle' + categoryState + '" type="button" data-toggle="dropdown">' +
         (task.category ? task.category : 'Category ') + '<span class="caret"></span></button><ul class="dropdown-menu">';
     for (category in download_categories) {
-        dropDown += '<li><a href="javascript:category_selected(\'' + task.id + '\', \'' + download_categories[category] + '\')" class="download_category">' + download_categories[category] + '</a></li>';
+        dropDown += '<li><a href="javascript:category_selected(\'' + task.id + '\', \'' + task.name + '\', \'' + download_categories[category] + '\')" class="download_category">' + download_categories[category] + '</a></li>';
     }
     dropDown += '</ul></div>';
 

premiumizer/templates/base.html
@@ -68,9 +68,9 @@
     </div>
 </div>
 {% block js_footer %}
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-            crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
             integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
             crossorigin="anonymous"></script>

premiumizer/templates/index.html
@@ -151,8 +151,10 @@
 
 {% block js_footer %}
     {{ super() }}
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/3.1.1/socket.io.js"
-        integrity="sha512-oFOCo2/3DtjrJG4N27BjSLQWoiBv171sK6a+JiWjp/7agxC2nCUP358AqzxkBUb5jX8g6CYLPdSKQTbC0weCwA=="
-        crossorigin="anonymous"></script>
-    <script type="text/javascript" src="{{ url_for('static', filename='js/control.js') }}?v=22"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.3.2/socket.io.min.js"
+        integrity="sha512-N5JbZMETkl+0SZh5l8OA2VxSfF120owfQyy54iAXTJIKuqAn3A9TrNz1ypB55o8aSU5MxaodEgv74wGUvgUynQ=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer">
+    </script>
+    <script type="text/javascript" src="{{ url_for('static', filename='js/control.js') }}?v=23"></script>
 {% endblock %}

commit 251f21a9f4ca61457933ac2ef9af7a19a33ce671

fix #331 caused by #322

premiumizer/premiumizer.py
@@ -1648,6 +1648,15 @@ def parse_tasks(transfers):
         size = ' '
         task = get_task(transfer['id'], name)
         try:
+            if 'download.php?id=' in name:
+                name = name.split("&f=", 1)[1]
+            if name.endswith('.torrent'):
+                name = name.split('.torrent', 1)[0]
+            elif name.endswith('.nzb'):
+                name = name.split('.nzb', 1)[0]
+        except:
+            pass
+        try:
             if 'ETA is' in transfer['message']:
                 eta = transfer['message'].split("ETA is", 1)[1]
             elif 'peers,' in transfer['message'] or 'peer,' in transfer['message'] or 'usenet,' in transfer['message']: